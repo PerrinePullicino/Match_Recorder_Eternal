c78508af2bf7d2a39200ceae10f18abd8d94928a
PerrinePullicino <perrinepullicino@protonmail.com>
2020-06-06 12:57:17 +0200

    Displacing create-react-app files to src/main folder

--- electron_renderer/src/main/App.tsx
+++ electron_renderer/src/main/App.tsx
@@ -5,20 +5,25 @@ import './App.scss';
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 
 const accessSpreadsheet = async () => {
-    const requiredDoc = new GoogleSpreadsheet('1ZYU5g58elbUSl5SEj8BScSnpNha0sTc7QsvjjkoCouQ');
+    const requiredDoc = new GoogleSpreadsheet(process.env.REACT_APP_SPREADSHEET_URL_ID);
     
     // If not working, see: https://github.com/theoephraim/node-google-spreadsheet/issues/117
     requiredDoc.useServiceAccountAuth({
         client_email: process.env.REACT_APP_GOOGLE_SERVICE_ACCOUNT_EMAIL,
         private_key: process.env.REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY,
     });
+
     await requiredDoc.loadInfo(); // loads document properties and worksheets
-    console.log(requiredDoc.title);
+    
+    console.log(requiredDoc.sheetsByIndex.map((doc: { title: string; }) => doc.title).includes("Decks"));
     // await requiredDoc.updateProperties({ title: 'Eternal Climbing' });
     
     // const sheet = requiredDoc.sheetsByIndex[0]; // or use doc.sheetsById[id]
     // console.log(sheet.title);
     // console.log(sheet.rowCount);
+    // const rows = await sheet.getRows();
+    // console.log(rows);
+    // await rows[1].delete(); // delete a row
 }
 
 
